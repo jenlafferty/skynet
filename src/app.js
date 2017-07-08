1eec355f372d80cf60b69a6f45e259d70d1cae82
Charles Lafferty <Charles.a.lafferty@gmail.com>
2017-07-07 22:05:12 -0400

    Added termintor api

--- src/app.js
+++ src/app.js
@@ -5,6 +5,22 @@ app.get('/', function (req, res) {
   res.send('Hello World! - Skynet!');
 });
 
+// Create the express router object for Photos
+var terminatorRouter = express.Router();
+
+terminatorRouter.get('/T800', function (req, res) {
+  var result = {
+    id: 'T800',
+    target: 'Sarah Connor',
+    destination_year: '1984',
+    manufactured: '2029',
+    design: 'Living tissue over a metal endoskeleton',
+  };
+  res.json(result);
+});
+
+app.use('/api/v1/terminator', terminatorRouter);
+
 app.listen(3005, function () {
   console.log('Example app listening on port 3005!');
 });
